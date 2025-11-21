2f7a8552bfebe625390d89bfd6418cbd652b977f
Christopher Howard <124425557+howardc830@users.noreply.github.com>
2025-11-20 19:23:34 -0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -13,10 +13,9 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "data/census.csv"
-data_path = os.path.join(project_path, "data", "census.csv")
-print(data_path)
-data = pd.read_csv(project_path) # your code here
+data = pd.read_csv("data/census.csv")
+
+#os.makedirs("model", exist_ok=True)
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
