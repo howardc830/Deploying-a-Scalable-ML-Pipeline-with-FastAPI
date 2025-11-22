69e7ac55ce57a840219bd9ba6eb5ca1dd851364e
Christopher Howard <124425557+howardc830@users.noreply.github.com>
2025-11-22 17:33:51 -0500

    Update local_api.py

--- local_api.py
+++ local_api.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = requests.get("http://127.0.0.1:8000/") # Your code here
+r = requests.get("http://127.0.0.1:8000/data/") # Your code here
 
 # TODO: print the status code
 print(f"Status Code: {r.status_code}")
